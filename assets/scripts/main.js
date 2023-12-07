f669b440c2948a773c770125ed5f67ea4fbd56fb
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2023-12-07 00:32:00 -0500

    Initial site creation

---
+++ assets/scripts/main.js
@@ -0,0 +1,141 @@
+//------------------------------Initial event handler on page loading----------------------------------------------
+window.onload = init;
+
+
+function init(){
+    
+    //read current page and log the name to the console for confirmation
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log(page);
+    
+    
+    //----had to create a logic statement to run different programs based on the page currently in use
+    //----kept breaking otherwise since all the js code is in one file
+    if (page == "Plans.html") {
+        var thisDay = new Date();
+    
+        startCal(thisDay);
+    } else if (page == "Art.html") {
+        var artArray = ["../../assets/images/cosplay1.jpg", "../../assets/images/cosplay2.jpg", "../../assets/images/cosplay3.png", "../../assets/images/cosplay4.jpg", "../../assets/images/cosplay5.jpg"];
+        var artDesc = "Zagreus Cosplay from Hades Video Game <br> Created 2022 <br> Eva Foam, Acrylic Paint, Cardboard, Sewn Fabric";                
+        var collabArray = ["../../assets/images/collab1.jpg", "../../assets/images/collab2.jpg", "../../assets/images/collab3.jpg", "../../assets/images/collab4.jpg", "../../assets/images/collab5.jpg", "../../assets/images/collab6.jpg", "../../assets/images/collab7.jpg", "../../assets/images/collab8.jpg", "../../assets/images/collab9.jpg", "../../assets/images/collab10.jpg", "../../assets/images/collab11.jpg", "../../assets/images/collab12.jpg"];
+        var collabDesc1  = "Stranger Things themed Halloween <br> Created 2022 <br> Tempura Paint, Cardboard, Paper";
+        var collabDesc2 = "Pokemon themed Halloween <br> Created 2023 <br> Tempura Paint, Cardboard, Paper";
+        var collabDesc = [collabDesc1, collabDesc1, collabDesc1, collabDesc1, collabDesc1, collabDesc1, collabDesc1, collabDesc1, collabDesc2, collabDesc2, collabDesc2, collabDesc2];
+        var commissArray = ["../../assets/images/commiss1.jpg", "../../assets/images/commiss2.jpg", "../../assets/images/commiss3.jpeg", "../../assets/images/commiss4.jpeg"];
+        var commissDesc = "Bleach Painted Shirts <br> Created 2022 <br> Bleach, Black T-Shirts";
+        buildArtGallery(artArray, artDesc, collabArray, collabDesc, commissArray, commissDesc);
+    }
+
+    
+
+
+
+}
+
+// ---------------------------Creating the calendar for the plans page------------------------------------
+function startCal(calDate) {
+    console.log("running the code for Plans.html");
+    var calHTML = "<table id='calendar_table'>";
+    calHTML += calCap(calDate);
+    calHTML += calTopRow();
+    calHTML += displayCalDays(calDate);
+    calHTML += "</table>";
+    document.getElementById("calendar").innerHTML = calHTML;
+}
+
+function calCap(calDate) {
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var thisMon = calDate.getMonth();
+    var thisYr = calDate.getFullYear();
+    var calDHTML = "<h1>" + months[thisMon] + " Plans</h1>";
+
+    //setting logic for the calendar description at bottom saying the months plans
+    if((thisMon === 10) && (thisYr === 2023) ){
+        console.log("nov 2023");
+        calDHTML += "<p>11/1-11/30: Working on Website. <br> 11/12-11/30: Working on 3D Models.</p>";
+        document.getElementById("calDescription").innerHTML = calDHTML;
+    } else if((thisMon === 11) && (thisYr ===2023)) {
+        console.log("dec 2023");
+        calDHTML += "<p>12/1-12/20: Working on Cosplay Projects. <br> 12/20-12/22: Holiday Matsuri Convention. <br> 12/23-12/31: Holiday Commissions and Projects.</p>";
+        document.getElementById("calDescription").innerHTML = calDHTML;
+    }
+    return "<caption>" + months[thisMon] + " " + thisYr + "</caption>";
+}
+
+function calTopRow() {
+    var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    var rowHTML = "<tr>";
+    for(var i =0; i<days.length; i++) {
+        rowHTML += "<th class='calDayNames'>" + days[i] + "</th>";
+    }
+    rowHTML += "</tr>";
+    return rowHTML;
+}
+
+function calDays(calDate) {
+    var numDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var thisYr = calDate.getFullYear();
+    var thisMon = calDate.getMonth();
+
+    if(thisYr % 4 == 0){
+        if((thisYr % 100 != 0) || (thisYr % 400 === 0)) {
+            numDays[1] = 29;
+        }
+    }
+    return numDays[thisMon];
+}
+
+function displayCalDays(calDate) {
+    var day  = new Date(calDate.getFullYear(), calDate.getMonth(), 1);
+    console.log(day);
+    var weekDay = day.getDay();
+    var dayHTML = "<tr>";
+    for(var i=0; i<weekDay; i++) {
+        dayHTML += "<td></td>";
+    }
+    var totDays = calDays(calDate);
+    var selectDay = calDate.getDate();
+    for(var i=1; i<=totDays; i++){
+        day.setDate(i);
+        weekDay = day.getDay();
+        if(weekDay === 0) dayHTML += "<tr>";
+        if (i === selectDay) {
+            dayHTML += "<td class='calDays' id='calToday'>" + i + "</td>";
+        }
+        else {
+            dayHTML += "<td class='calDays'>" + i + "</td>";
+        }
+        if (weekDay === 6) dayHTML += "</tr>";
+    }
+
+    return dayHTML;
+}
+//----------------------------------------------------------------------------------------
+//-----------------------------Creating the image gallery---------------------------------
+function buildArtGallery(artA, artD, collabA, collabD, commissA, commissD) {
+    console.log("Running the code for Art.html");
+    var artHTML = "";
+    for(var i = 0; i < artA.length; i++) {
+        artHTML += "<div class='galleryItem'>"
+        artHTML += "<a target='_blank' href='" + artA[i] + "'>";
+        artHTML += "<img src='" + artA[i] + "' alt='Image Gallery'></a><div><p>" + artD + "</p></div></div>";
+        document.getElementById("artGallery").innerHTML = artHTML;
+    }
+    artHTML = "";
+    for(var i = 0; i < collabA.length; i++) {
+        artHTML += "<div class='galleryItem'>"
+        artHTML += "<a target='_blank' href='" + collabA[i] + "'>";
+        artHTML += "<img src='" + collabA[i] + "' alt='Image Gallery'></a><div><p>" + collabD[i] + "</p></div></div>";
+        document.getElementById("collabGallery").innerHTML = artHTML;
+    }
+    artHTML = "";
+    for(var i = 0; i < commissA.length; i++) {
+        artHTML += "<div class='galleryItem'>"
+        artHTML += "<a target='_blank' href='" + commissA[i] + "'>";
+        artHTML += "<img src='" + commissA[i] + "' alt='Image Gallery'></a><div><p>" + commissD + "</p></div></div>";
+        document.getElementById("commissGallery").innerHTML = artHTML;
+    }
+}
+//--------------------------------------------------------------------------------------------
